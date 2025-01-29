0aa1d38efc51fcca7ba5019a9a94e0380f2a882d
Ichrak Aguir <ichrakaguir123@gmail.com>
2025-01-29 02:31:33 +0100

    [BackEnd][Models] add user && blog files

---
+++ blog-crud/models/Blog.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
